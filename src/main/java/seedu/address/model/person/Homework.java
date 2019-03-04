c61bdbc2dbb531f4e1721f449d4b7529461e0b76
LimZhengJieBenjamin <benjamin_blzj@hotmail.com>
2019-03-05 00:25:31 +0800

    Created the Homework class which will be used for storing homework in a to do list.

---
+++ src/main/java/seedu/address/model/person/Homework.java
@@ -0,0 +1,46 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+public class Homework {
+    String moduleCode;
+    String homeworkName;
+    String deadline;
+
+    public static final String MESSAGE_CONSTRAINTS = "Homework should be inputted as MODULECODE; "
+                                                       + "HOMEWORKNAME; DEADLINE";
+    //TOADD REGEX to only allow numbers, alphabets and spaces
+
+    public Homework (String moduleCode, String homeworkName, String deadline) {
+        requireNonNull(moduleCode);
+        this.moduleCode = moduleCode;
+
+        requireNonNull(homeworkName);
+        this.homeworkName = homeworkName;
+
+        requireNonNull(deadline);
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String toString() {
+        return moduleCode + "; " + homeworkName + "; " + deadline + "; ";
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Homework // instanceof handles nulls
+                && moduleCode.equals(((Homework) other).moduleCode)
+                && homeworkName.equals(((Homework) other).homeworkName)
+                && deadline.equals(((Homework) other).deadline));
+    }
+
+    @Override
+    public int hashCode() {
+        String value = moduleCode + " " + homeworkName + " " + deadline;
+        return value.hashCode();
+    }
+
+}
