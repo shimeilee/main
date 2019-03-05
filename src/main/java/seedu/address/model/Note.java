e9c65a528c86ed181de6386d2bbacd4f7238392d
shimeilee <30607394+shimeilee@users.noreply.github.com>
2019-03-05 14:13:22 +0800

    Create Note class and constructor

---
+++ src/main/java/seedu/address/model/Note.java
@@ -0,0 +1,25 @@
+package seedu.address.model;
+
+/**
+ * Represents a Note in UltiStudent - Notes Keeping.
+ */
+public class Note {
+    // Notes fields
+    private final String modulename;
+    private final String notename;
+    
+    // Data fields
+    // private File content;
+    
+    /**
+     * Constructor
+     * Every field must be present and not null.
+     */
+    public Note(String moduleName, String noteName) {
+        modulename = moduleName;
+        notename = noteName;
+        // content = new File(new );
+    }
+    
+}
+
