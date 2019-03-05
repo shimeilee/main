f6e39d871ddaa46ee490426d0b1f3755f3773f49
shimeilee <30607394+shimeilee@users.noreply.github.com>
2019-03-05 19:47:10 +0800

    Minor bug fixes for trailing whitespaces

--- src/main/java/seedu/address/model/person/Note.java
+++ src/main/java/seedu/address/model/person/Note.java
@@ -6,14 +6,14 @@ import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
  * Represents a Note in UltiStudent - Notes Keeping.
  */
 public class Note {
-    
+
     // Notes fields
     private String modulename;
     private String notename;
-    
+
     // Data fields
     // private File content;
-    
+
     /**
      * Every field must be present and not null.
      */
@@ -23,7 +23,7 @@ public class Note {
         notename = noteName;
         // content = new File(new );
     }
-    
+
     @Override
     /**
      * Returns a String containing the note name.
@@ -31,7 +31,7 @@ public class Note {
     public String toString() {
         return modulename + " " + notename;
     }
-    
+
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
@@ -39,9 +39,7 @@ public class Note {
                 && modulename.equals(((Note) other).modulename) // state check
                 && notename.equals(((Note) other).notename)); // state check
     }
-    
+
     @Override
     public int hashCode() { return notename.hashCode(); }
 }
-
-
