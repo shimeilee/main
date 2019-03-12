5ff8a7826342d63d6a13d947e9c01ad6bedb20dc
NgWeiXin <xavier_ng@icloud.com>
2019-03-12 13:28:41 +0800

    Commented unused imports in IconListPanel

--- src/main/java/seedu/address/ui/IconListPanel.java
+++ src/main/java/seedu/address/ui/IconListPanel.java
@@ -1,15 +1,19 @@
 package seedu.address.ui;
-
+/*
 import java.util.Objects;
 import java.util.function.Consumer;
-import java.util.logging.Logger;
+*/
 
+import java.util.logging.Logger;
+/*
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
+*/
+
 import seedu.address.commons.core.LogsCenter;
 
 /**
