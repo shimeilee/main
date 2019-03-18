7cd0981b78495de24e52994c0055ee7101dca5cf
LimZhengJieBenjamin <benjamin_blzj@hotmail.com>
2019-03-19 00:55:48 +0800

    Added regex to Year class.

--- src/main/java/seedu/address/model/person/Year.java
+++ src/main/java/seedu/address/model/person/Year.java
@@ -1,7 +1,7 @@
 package seedu.address.model.person;
 
 import static java.util.Objects.requireNonNull;
-//import static seedu.address.commons.util.AppUtil.checkArgument;
+import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
  * Represents a Date's year which is used to denote deadlines for a Homework.
@@ -9,25 +9,25 @@ import static java.util.Objects.requireNonNull;
 public class Year {
     public static final String MESSAGE_CONSTRAINTS =
             "Year should only contain numbers, and should be 4 digits long.";
-    //public static final String VALIDATION_REGEX = "\\d{3,}";
+    public static final String VALIDATION_REGEX = "\\d{4}";
     public final String value;
 
     /**
-     * Constructs a {@code Phone}.
+     * Constructs a {@code Year}.
      *
-     * @param phone A valid phone number.
+     * @param year A valid year number.
      */
     public Year(String year) {
         requireNonNull(year);
-        //checkArgument(isValidPhone(phone), MESSAGE_CONSTRAINTS);
+        checkArgument(isValidYear(year), MESSAGE_CONSTRAINTS);
         value = year;
     }
 
     /**
-     * Returns true if a given string is a valid phone number.
+     * Returns true if a given string is a valid year number.
      */
-    public static boolean isValidPhone(String test) {
-        return true;
+    public static boolean isValidYear(String test) {
+        return test.matches(VALIDATION_REGEX);
     }
 
     @Override
