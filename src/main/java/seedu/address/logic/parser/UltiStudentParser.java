7406ad65b535b40c02cb5f3307ac5b66ae382872
andrewscw <andryscw@gmail.com>
2019-03-26 11:59:44 +0800

    Resolve conflict

--- src/main/java/seedu/address/logic/parser/UltiStudentParser.java
+++ src/main/java/seedu/address/logic/parser/UltiStudentParser.java
@@ -14,12 +14,9 @@ import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCapEntryCommand;
 import seedu.address.logic.commands.DeleteCommand;
-<<<<<<< HEAD
 import seedu.address.logic.commands.EditCapEntryByModuleCodeCommand;
 import seedu.address.logic.commands.EditCapEntryCommand;
-=======
 import seedu.address.logic.commands.DeleteHomeworkCommand;
->>>>>>> 39df07c66104447385f5854a02ceef0d3609d598
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
