4302980496ea00b73ac5d44cd901e1e8e102aec4
LimZhengJieBenjamin <benjamin_blzj@hotmail.com>
2019-04-13 20:22:35 +0800

    Editted comments within command classes.

--- src/main/java/seedu/ultistudent/logic/parser/FindModuleCommandParser.java
+++ src/main/java/seedu/ultistudent/logic/parser/FindModuleCommandParser.java
@@ -9,14 +9,14 @@ import seedu.ultistudent.logic.parser.exceptions.ParseException;
 import seedu.ultistudent.model.homework.ModuleCodeContainsKeywordsPredicate;
 
 /**
- * Parses input arguments and creates a new FindCommand object
+ * Parses input arguments and creates a new FindCommand object.
  */
 public class FindModuleCommandParser implements Parser<FindModuleCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the FindCommand
      * and returns an FindCommand object for execution.
-     * @throws ParseException if the user input does not conform the expected format
+     * @throws ParseException if the user input does not conform the expected format.
      */
     public FindModuleCommand parse(String args) throws ParseException {
         String trimmedArgs = args.trim();
