a598a719f8b23e6ec4fe652a8900fa64a545f11e
LimZhengJieBenjamin <benjamin_blzj@hotmail.com>
2019-04-14 14:51:43 +0800

    Added ListHomeworkCommand class.

---
+++ src/main/java/seedu/ultistudent/logic/commands/ListHomeworkCommand.java
@@ -0,0 +1,25 @@
+package seedu.ultistudent.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.ultistudent.model.Model.PREDICATE_SHOW_ALL_CAP_ENTRIES;
+import static seedu.ultistudent.model.Model.PREDICATE_SHOW_ALL_HOMEWORK;
+
+import seedu.ultistudent.logic.CommandHistory;
+import seedu.ultistudent.model.Model;
+
+/**
+ * Lists all homework in the Homework Manager to the user.
+ */
+public class ListHomeworkCommand extends Command {
+
+    public static final String COMMAND_WORD = "list-hw";
+
+    public static final String MESSAGE_SUCCESS = "Listed all homeworks.";
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) {
+        requireNonNull(model);
+        model.updateFilteredHomeworkList(PREDICATE_SHOW_ALL_HOMEWORK);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
