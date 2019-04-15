2399d7d8680e6687195a435fc4090d3c30ce6990
shimeilee <30607394+shimeilee@users.noreply.github.com>
2019-04-15 23:40:56 +0800

    whitespaces fix

--- src/main/java/seedu/ultistudent/model/util/SampleDataUtil.java
+++ src/main/java/seedu/ultistudent/model/util/SampleDataUtil.java
@@ -31,149 +31,133 @@ public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         return new Person[] {
                 new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                        new Address("Blk 30 Geylang Street 29, #06-40"),
-                        getTagSet("friends")),
+                    new Address("Blk 30 Geylang Street 29, #06-40"),
+                    getTagSet("friends")),
                 new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                        new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                        getTagSet("colleagues", "friends")),
+                    new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
+                    getTagSet("colleagues", "friends")),
                 new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                        new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                        getTagSet("neighbours")),
+                    new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
+                    getTagSet("neighbours")),
                 new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                        new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                        getTagSet("family")),
+                    new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
+                    getTagSet("family")),
                 new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                        new Address("Blk 47 Tampines Street 20, #17-35"),
-                        getTagSet("classmates")),
+                    new Address("Blk 47 Tampines Street 20, #17-35"),
+                    getTagSet("classmates")),
                 new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                        new Address("Blk 45 Aljunied Street 85, #11-31"),
-                        getTagSet("colleagues"))
+                    new Address("Blk 45 Aljunied Street 85, #11-31"),
+                    getTagSet("colleagues"))
         };
     }
 
     public static CapEntry[] getSampleCapEntryList() {
         return new CapEntry[]{
                 new CapEntry(new ModuleCode("CS1010"), new ModuleGrade("B+"), new ModuleCredits("4"),
-                        new ModuleSemester("Y1S1")),
+                    new ModuleSemester("Y1S1")),
                 new CapEntry(new ModuleCode("CS1231"), new ModuleGrade("A-"), new ModuleCredits("4"),
-                        new ModuleSemester("Y1S1")),
+                    new ModuleSemester("Y1S1")),
                 new CapEntry(new ModuleCode("GER1000"), new ModuleGrade("A-"), new ModuleCredits("4"),
-                        new ModuleSemester("Y1S1")),
+                    new ModuleSemester("Y1S1")),
                 new CapEntry(new ModuleCode("MA1521"), new ModuleGrade("B+"),
-                        new ModuleCredits("4"),
-                        new ModuleSemester("Y1S1")),
+                    new ModuleCredits("4"),
+                    new ModuleSemester("Y1S1")),
                 new CapEntry(new ModuleCode("MA1101R"), new ModuleGrade("B+"),
-                        new ModuleCredits("4"),
-                        new ModuleSemester("Y1S1")),
+                    new ModuleCredits("4"),
+                    new ModuleSemester("Y1S1")),
                 new CapEntry(new ModuleCode("PC1221"), new ModuleGrade("B+"),
-                        new ModuleCredits("4"),
-                        new ModuleSemester("Y1S2")),
+                    new ModuleCredits("4"),
+                    new ModuleSemester("Y1S2")),
                 new CapEntry(new ModuleCode("PC1222"), new ModuleGrade("A-"),
-                        new ModuleCredits("4"),
-                        new ModuleSemester("Y1S2")),
+                    new ModuleCredits("4"),
+                    new ModuleSemester("Y1S2")),
                 new CapEntry(new ModuleCode("CS2040"), new ModuleGrade("A-"),
-                        new ModuleCredits("4"),
-                        new ModuleSemester("Y1S2")),
+                    new ModuleCredits("4"),
+                    new ModuleSemester("Y1S2")),
                 new CapEntry(new ModuleCode("CS2030"), new ModuleGrade("B+"), new ModuleCredits("4"),
-                        new ModuleSemester("Y1S2")),
+                    new ModuleSemester("Y1S2")),
                 new CapEntry(new ModuleCode("CS2101"), new ModuleGrade("B+"), new ModuleCredits("4"),
-                        new ModuleSemester("Y1S2")),
+                    new ModuleSemester("Y1S2")),
                 new CapEntry(new ModuleCode("CS2103T"), new ModuleGrade("B+"),
-                        new ModuleCredits("4"),
-                        new ModuleSemester("Y1S2")),
+                    new ModuleCredits("4"),
+                    new ModuleSemester("Y1S2")),
                 new CapEntry(new ModuleCode("CS2102"), new ModuleGrade("A-"),
-                        new ModuleCredits("4"),
-                        new ModuleSemester("Y2S1")),
+                    new ModuleCredits("4"),
+                    new ModuleSemester("Y2S1")),
                 new CapEntry(new ModuleCode("CS2104"), new ModuleGrade("A-"),
-                        new ModuleCredits("4"),
-                        new ModuleSemester("Y2S1")),
+                    new ModuleCredits("4"),
+                    new ModuleSemester("Y2S1")),
                 new CapEntry(new ModuleCode("CS2105"), new ModuleGrade("B+"),
-                        new ModuleCredits("4"),
-                        new ModuleSemester("Y2S1")),
+                    new ModuleCredits("4"),
+                    new ModuleSemester("Y2S1")),
                 new CapEntry(new ModuleCode("CS2106"), new ModuleGrade("B+"),
-                        new ModuleCredits("4"),
-                        new ModuleSemester("Y2S1")),
+                    new ModuleCredits("4"),
+                    new ModuleSemester("Y2S1")),
                 new CapEntry(new ModuleCode("CS2107"), new ModuleGrade("B+"),
-                        new ModuleCredits("4"),
-                        new ModuleSemester("Y2S1")),
+                    new ModuleCredits("4"),
+                    new ModuleSemester("Y2S1")),
                 new CapEntry(new ModuleCode("CS3230"), new ModuleGrade("A-"),
-                        new ModuleCredits("4"),
-                        new ModuleSemester("Y2S2")),
+                    new ModuleCredits("4"),
+                    new ModuleSemester("Y2S2")),
                 new CapEntry(new ModuleCode("CS3223"), new ModuleGrade("A-"),
-                        new ModuleCredits("4"),
-                        new ModuleSemester("Y2S2")),
+                    new ModuleCredits("4"),
+                    new ModuleSemester("Y2S2")),
                 new CapEntry(new ModuleCode("ST2334"), new ModuleGrade("B+"),
-                        new ModuleCredits("4"),
-                        new ModuleSemester("Y2S2")),
+                    new ModuleCredits("4"),
+                    new ModuleSemester("Y2S2")),
                 new CapEntry(new ModuleCode("ST2131"), new ModuleGrade("B+"),
-                        new ModuleCredits("4"),
-                        new ModuleSemester("Y2S2"))
+                    new ModuleCredits("4"),
+                    new ModuleSemester("Y2S2"))
         };
     }
 
     public static Note[] getSampleNotes() {
         return new Note[]{
                 new Note(new ModuleCode("CS2103T"), new NoteName("Personal Project Portfolio"), new Content("Things to "
-                        + "include:\n1. Overview\n2. Summary of Contribution\n"
-                        + "3. Contribution to User Guide\n4. Contribution to Developer Guide")),
+                    + "include:\n1. Overview\n2. Summary of Contribution\n"
+                    + "3. Contribution to User Guide\n4. Contribution to Developer Guide")),
                 new Note(new ModuleCode("CS2103T"), new NoteName("Feedback"), new Content("Orientate the user during the"
-                        + " introduction.\nWin their hearts within the first 30s.\nWhy do the user need UltiStudent?"
-                        + "\n\n3mins per presenter")),
+                    + " introduction.\nWin their hearts within the first 30s.\nWhy do the user need UltiStudent?"
+                    + "\n\n3mins per presenter")),
                 new Note(new ModuleCode("CS2101"), new NoteName("Week 1 Notes"), new Content("Things to "
-                        + "include:\n1. Overview\n2. Summary of Class\n")),
-                new Note(new ModuleCode("CS2101"), new NoteName("Feedback for " +
-                        "Pitch"),
-                        new Content("Orientate the user during the"
-                                + " introduction.\nWin their hearts within the first 30s.\nWhy do the user need UltiStudent?"
-                                + "\n\n3mins per presenter")),
+                    + "include:\n1. Overview\n2. Summary of Class\n")),
+                new Note(new ModuleCode("CS2101"), new NoteName("Feedback for Pitch"),
+                    new Content("Orientate the user during the"
+                    + " introduction.\nWin their hearts within the first 30s.\nWhy do the user need UltiStudent?"
+                    + "\n\n3mins per presenter")),
                 new Note(new ModuleCode("CS1101S"), new NoteName("Week 1 Notes"),
-                        new Content("Things to "
-                                + "include:\n1. Overview\n2. Summary of Class\n")),
+                    new Content("Things to " + "include:\n1. Overview\n2. Summary of Class\n")),
                 new Note(new ModuleCode("CS1101S"), new NoteName("Week 2 Notes"),
-                        new Content("Things to "
-                                + "include:\n1. Overview\n2. Summary of Class\n")),
+                        new Content("Things to " + "include:\n1. Overview\n2. Summary of Class\n")),
                 new Note(new ModuleCode("CS1101S"), new NoteName("Week 3 Notes"),
-                        new Content("Things to "
-                                + "include:\n1. Overview\n2. Summary of Class\n")),
+                    new Content("Things to " + "include:\n1. Overview\n2. Summary of Class\n")),
                 new Note(new ModuleCode("CS1101S"), new NoteName("Week 4 Notes"),
-                        new Content("Things to "
-                                + "include:\n1. Overview\n2. Summary of Class\n")),
+                    new Content("Things to " + "include:\n1. Overview\n2. Summary of Class\n")),
                 new Note(new ModuleCode("CS2030"), new NoteName("Week 1 Notes"),
-                        new Content("Things to "
-                                + "include:\n1. Overview\n2. Summary of Class\n")),
+                    new Content("Things to " + "include:\n1. Overview\n2. Summary of Class\n")),
                 new Note(new ModuleCode("CS2030"), new NoteName("Week 2 Notes"),
-                        new Content("Things to "
-                                + "include:\n1. Overview\n2. Summary of Class\n")),
+                    new Content("Things to " + "include:\n1. Overview\n2. Summary of Class\n")),
                 new Note(new ModuleCode("CS2030"), new NoteName("Week 3 Notes"),
-                        new Content("Things to "
-                                + "include:\n1. Overview\n2. Summary of Class\n")),
+                    new Content("Things to " + "include:\n1. Overview\n2. Summary of Class\n")),
                 new Note(new ModuleCode("CS2030"), new NoteName("Week 4 Notes"),
-                        new Content("Things to "
-                                + "include:\n1. Overview\n2. Summary of Class\n")),
+                    new Content("Things to " + "include:\n1. Overview\n2. Summary of Class\n")),
                 new Note(new ModuleCode("CS2040"), new NoteName("Week 1 Notes"),
-                        new Content("Things to "
-                                + "include:\n1. Overview\n2. Summary of Class\n")),
+                    new Content("Things to " + "include:\n1. Overview\n2. Summary of Class\n")),
                 new Note(new ModuleCode("CS2040"), new NoteName("Week 2 Notes"),
-                        new Content("Things to "
-                                + "include:\n1. Overview\n2. Summary of Class\n")),
+                    new Content("Things to " + "include:\n1. Overview\n2. Summary of Class\n")),
                 new Note(new ModuleCode("CS2040"), new NoteName("Week 3 Notes"),
-                        new Content("Things to "
-                                + "include:\n1. Overview\n2. Summary of Class\n")),
+                    new Content("Things to " + "include:\n1. Overview\n2. Summary of Class\n")),
                 new Note(new ModuleCode("CS2040"), new NoteName("Week 4 Notes"),
-                        new Content("Things to "
-                                + "include:\n1. Overview\n2. Summary of Class\n")),
+                    new Content("Things to " + "include:\n1. Overview\n2. Summary of Class\n")),
                 new Note(new ModuleCode("CS2102"), new NoteName("Week 1 Notes"),
-                        new Content("Things to "
-                                + "include:\n1. Overview\n2. Summary of Class\n")),
+                    new Content("Things to " + "include:\n1. Overview\n2. Summary of Class\n")),
                 new Note(new ModuleCode("CS2102"), new NoteName("Week 2 Notes"),
-                        new Content("Things to "
-                                + "include:\n1. Overview\n2. Summary of Class\n")),
+                    new Content("Things to " + "include:\n1. Overview\n2. Summary of Class\n")),
                 new Note(new ModuleCode("CS2102"), new NoteName("Week 3 Notes"),
-                        new Content("Things to "
-                                + "include:\n1. Overview\n2. Summary of Class\n")),
+                    new Content("Things to " + "include:\n1. Overview\n2. Summary of Class\n")),
                 new Note(new ModuleCode("CS2102"), new NoteName("Week 4 Notes"),
-                        new Content("Things to "
-                                + "include:\n1. Overview\n2. Summary of Class\n"))
+                    new Content("Things to " + "include:\n1. Overview\n2. " +
+                        "Summary of Class\n"))
         };
     }
 
